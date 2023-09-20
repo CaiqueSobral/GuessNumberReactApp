46e9e254590bba89e82a46e60df11e95c586c644
Kiki <caiquelsobral@gmail.com>
2023-09-20 19:45:09 -0300

    Feat: Add tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,10 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export function App() {
   return (
-    <View style={styles.container}>
+    <View className="flex-1 bg-white items-center justify-center">
       <Text>Open up App.tsx to start working on your app!</Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
 
 registerRootComponent(App);
