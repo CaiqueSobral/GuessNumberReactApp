7a6fb11c11854b7e69d714d2f1d4950e40be5510
Kiki <kiki@ibm.com>
2023-09-24 14:25:43 -0300

    Feat: Reset the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { registerRootComponent } from 'expo';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
 import HomePage from './pages/HomePage';
 
 export function App() {
--- src/pages/homePage.tsx
+++ src/pages/homePage.tsx
@@ -1,14 +1,11 @@
 import React from 'react';
 import { View } from 'react-native';
 import PrimaryButton from '../components/PrimaryButton';
-import BackgroundImageRender from '../components/BackgroundImageRender';
 
 export default function HomePage() {
   return (
     <View className="flex-1 bg-neutral-500">
-      <BackgroundImageRender>
-        <PrimaryButton>Play</PrimaryButton>
-      </BackgroundImageRender>
+      <PrimaryButton>Play</PrimaryButton>
     </View>
   );
 }
