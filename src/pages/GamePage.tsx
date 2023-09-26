9f150e2acb6bcd99e400944bdc5a696c4624c46f
Kiki <caiquelsobral@gmail.com>
2023-09-26 10:59:43 -0300

    Refactor: Remove unused log

--- src/pages/GamePage.tsx
+++ src/pages/GamePage.tsx
@@ -72,8 +72,6 @@ export default function GamePage({ navigation }: any) {
       return;
     }
 
-    console.log(randomNumber);
-
     if (chosenNumber === randomNumber) {
       resetInputHandler();
       resetGuessedNumbersList();
