7d839ae54e5e412446873cd953c9ca5a50b4d599
Kiki <caiquelsobral@gmail.com>
2023-09-26 08:50:53 -0300

    Feat: Add Game Over page

---
+++ src/pages/GameOverPage.tsx
@@ -0,0 +1,17 @@
+import PrimaryButton from '../components/PrimaryButton';
+import React from 'react';
+import { Text, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+export default function GameOverPage() {
+  return (
+    <SafeAreaView className="flex-1 justify-center p-[2%]">
+      <View className="self-center w-[90%] h-[30%] flex-col items-center justify-center rounded-xl bg-primary shadow-2xl opacity-95">
+        <Text className="text-3xl mb-16 text-secondary font-bold">
+          You Guessed It!!! Nice
+        </Text>
+        <PrimaryButton>Play Again</PrimaryButton>
+      </View>
+    </SafeAreaView>
+  );
+}
