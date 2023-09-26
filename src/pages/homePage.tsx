18f1c7a197e7b876ba7e8fe5c7c384f19e574673
Kiki <caiquelsobral@gmail.com>
2023-09-26 10:21:14 -0300

    Feat: Add navigation to "play" button

--- src/pages/homePage.tsx
+++ src/pages/homePage.tsx
@@ -1,17 +1,21 @@
 import React from 'react';
 import { Text, View } from 'react-native';
 import PrimaryButton from '../components/PrimaryButton';
-import { LinearGradient } from 'expo-linear-gradient';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { useNavigation } from '@react-navigation/native';
 
 export default function HomePage() {
+  const navigation = useNavigation();
   return (
-    <View className="flex-1 justify-center p-[2%]">
+    <SafeAreaView className="flex-1 justify-center p-[2%]">
       <View className="self-center w-[90%] h-[30%] flex-col items-center justify-center rounded-xl bg-primary shadow-2xl opacity-95">
         <Text className="text-3xl mb-16 text-secondary font-bold">
           Guess The Number
         </Text>
-        <PrimaryButton>Play</PrimaryButton>
+        <PrimaryButton onPress={() => navigation.navigate('GamePage' as never)}>
+          Play
+        </PrimaryButton>
       </View>
-    </View>
+    </SafeAreaView>
   );
 }
