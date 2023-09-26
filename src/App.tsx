646463c87a8e104d09d089d8e6ccfeb59a933657
Kiki <caiquelsobral@gmail.com>
2023-09-26 00:44:05 -0300

    Feat: Add some functionality and Safe Area Context

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,22 @@ import HomePage from './pages/HomePage';
 import GamePage from './pages/GamePage';
 import { LinearGradient } from 'expo-linear-gradient';
 import { ImageBackground, StyleSheet } from 'react-native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 export function App() {
   return (
-    <LinearGradient colors={['#4e0329', '#ddb52f']} className="flex-1">
-      <ImageBackground
-        source={require('../assets/background-image.png')}
-        resizeMode="cover"
-        className="flex-1"
-        imageStyle={{ opacity: 0.15 }}
-      >
-        <GamePage />
-      </ImageBackground>
-    </LinearGradient>
+    <SafeAreaProvider>
+      <LinearGradient colors={['#4e0329', '#ddb52f']} className="flex-1">
+        <ImageBackground
+          source={require('../assets/background-image.png')}
+          resizeMode="cover"
+          className="flex-1"
+          imageStyle={{ opacity: 0.15 }}
+        >
+          <GamePage />
+        </ImageBackground>
+      </LinearGradient>
+    </SafeAreaProvider>
   );
 }
 
--- src/pages/GamePage.tsx
+++ src/pages/GamePage.tsx
@@ -1,20 +1,43 @@
 import PrimaryButton from '../components/PrimaryButton';
 import React, { useState } from 'react';
-import { TextInput, View, Text, Alert } from 'react-native';
+import { TextInput, View, Text, Alert, FlatList } from 'react-native';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+
+type GuessesNumbers = {
+  guessedNumber: number;
+  id: number;
+};
+
+const randomNumber = Math.floor(Math.random() * 99) + 1;
 
 export default function GamePage() {
-  const randomNumber = Math.floor(Math.random() * 99) + 1;
+  const insets = useSafeAreaInsets();
   const [enteredNumber, setEnteredNumber] = useState('');
+  const [guessedNumbers, setGuessedNumbers] = useState<Array<GuessesNumbers>>(
+    [],
+  );
 
-  function numberInputHandler(enteredText: string) {
+  const addGuessedNumber = (guessedNumber: number) => {
+    setGuessedNumbers((oldGuessedNumbers) => {
+      return [
+        {
+          guessedNumber: guessedNumber,
+          id: guessedNumbers.length + 1,
+        },
+        ...oldGuessedNumbers,
+      ];
+    });
+  };
+
+  const numberInputHandler = (enteredText: string) => {
     setEnteredNumber(enteredText);
-  }
+  };
 
-  function resetInputHandler() {
+  const resetInputHandler = () => {
     setEnteredNumber('');
-  }
+  };
 
-  function confirmInputHandler() {
+  const confirmInputHandler = () => {
     const chosenNumber = parseInt(enteredNumber);
 
     if (isNaN(chosenNumber) || chosenNumber <= 0 || chosenNumber > 99) {
@@ -23,13 +46,23 @@ export default function GamePage() {
         'Number has to be a number between 1 and 99',
         [{ text: 'Okay', style: 'destructive', onPress: resetInputHandler }],
       );
-      return;
     }
-  }
+
+    console.log(randomNumber);
+
+    if (chosenNumber === randomNumber) console.log('You guessed it!!!!!');
+
+    addGuessedNumber(chosenNumber);
+    return;
+  };
 
   return (
-    <View className="flex-1 justify-center p-[5%]">
-      <View className="self-center w-[90%] h-[30%] mt-[-100%] flex-col items-center justify-center rounded-xl bg-[#3b021f] shadow-2xl">
+    <View className="flex-1 p-[2%]">
+      <View
+        className={`self-center w-[90%] h-[30%] mt-[${Math.floor(
+          insets.top,
+        )}] flex-col items-center justify-center rounded-xl bg-[#3b021f] shadow-2xl`}
+      >
         <Text className="text-[#ddb52f] text-3xl">Make Your Guess</Text>
         <TextInput
           maxLength={2}
@@ -42,6 +75,19 @@ export default function GamePage() {
         ></TextInput>
         <PrimaryButton onPress={confirmInputHandler}>Confirm</PrimaryButton>
       </View>
+      <FlatList
+        data={guessedNumbers}
+        className="flex-1 mt-[5%]"
+        renderItem={(guessedNumbers) => {
+          return (
+            <View className="w-[90%] self-center bg-[#ddb52f] border-2 border-slate-900 mb-2 rounded-full shadow-2xl">
+              <Text className="text-center text-lg">{`The number is actually ${
+                parseInt(enteredNumber) > randomNumber ? 'bigger' : 'lower'
+              } than #${guessedNumbers.item.guessedNumber}`}</Text>
+            </View>
+          );
+        }}
+      />
     </View>
   );
 }
